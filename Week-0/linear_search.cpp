c8e3652527afa842407b0c6f85ac75c7065c4806
Jigyasu <jigyasumang@gmail.com>
2021-07-18 10:41:07 +0530

    Week-1

---
+++ Week-0/linear_search.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+    	int n, key, comp = 0;
+    	bool flag = false;
+    	cin>>n;
+    	cin>>key;
+    	vector<int> v(n);
+
+    	for(int i=0; i<n; i++)
+    	{
+    		cin>>v[i];
+    	}
+
+    	for(int i=0; i<n; i++)
+    	{
+    		if(v[i] == key)
+    		{
+    			comp++;
+    			flag = true;
+    			break;
+    		}
+    		comp++;
+    	}
+
+    	(flag == true) ? (cout<<"Key is Present in Array"<<endl ) : (cout<<"Key is Not Present in an Array"<<endl );
+
+    	cout<<"No. of Comparison = "<<comp<<endl;
+    }
+    return 0;
+}
